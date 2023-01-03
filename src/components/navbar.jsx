915664497b9750b46435c57ac26f071a7c60f41a
Isaack Mungui <munguiisaack@gmail.com>
2023-01-03 15:47:23 +0300

    Removed unused import in navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
 function NavBar() {
     return (
-        <div className="fixed left-0 right-0 top-0 h-16 shadow-md border-b-2 border-gray-100 bg-gray-900">
+        <div className="lg:w-100 fixed left-0 right-0 top-0 h-16 shadow-md border-b-2 border-gray-100 bg-gray-900">
             <nav className="flex items-center container mx-auto h-full justify-between p-6">
                 <h1 className="font-semibold uppercase text-lg text-gray-200">
                     VenuSpot
                 </h1>
-                <div>
+                <div className='decoration-white'>
                     <ul className="flex items-center space-x-10 text-sm">
-                        <li className="text-gray-400 hover:text-gray-100">Home</li>
-                        <li className="text-gray-400 hover:text-gray-100">Browse Venues</li>
-                        <li className="text-gray-400 hover:text-gray-100">Add Listing</li>
+                        <li className="text-gray-400 text-lg hover:text-gray-100"><a href='#'>Home</a></li>
+                        <li className="text-gray-400 text-lg hover:text-gray-100"><a href='#'>Browse Venues</a></li>
+                        <li className="text-gray-400 text-lg hover:text-gray-100"><a href='#'>Add Listing</a></li>
                     </ul>
                 </div>
             </nav>
