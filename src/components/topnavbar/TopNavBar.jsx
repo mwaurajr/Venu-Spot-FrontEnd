7331fb257c09ea3ad7e2d81f2decd899570ca4f1
timothymureithi <timothy.mureithi@student.moringaschool.com>
2023-01-06 13:51:39 +0300

    top-navbar/manager

--- src/components/topnavbar/TopNavBar.jsx
+++ src/components/topnavbar/TopNavBar.jsx
@@ -10,23 +10,26 @@ function TopNavBar(props) {
         <span className="mainlogo">Manager Dashboard</span>
       </div>
       <div className="topnavRight">
-          <div className="topnavbarIconContainer">
-            <Language />
-            <div className="dropdown-content">
-              <p>Lorem</p>
-            </div>
-            <div className="topnavbarIconContainer">
-            <Settings />
-            <div className="dropdown-content">
-              <p>Tim</p>
-              <p>Immaculate</p>
-              <p>Isaack</p>
-            </div>
+        <div className="topnavbarIconContainer">
+          <Language />
+          <div className="dropdown-content">
+            <p>Lorem</p>
           </div>
+        </div>
+        <div className="topnavbarIconContainer">
+          <Settings />
+          <div className="dropdown-content">
+            <p>Test</p>
+            <p>Test</p>
           </div>
+        </div>
+        <div className="topnavbarIconContainer">
+          <NotificationsNone />
+          <span className="topnavIconBadge">2</span>
+        </div>
       </div>
-
-      </div>
+    </div>
+  </div>
   );
 }
 
