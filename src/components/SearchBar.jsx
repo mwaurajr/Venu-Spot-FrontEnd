d0d436be0616c343de0326efe251feb222c8ac48
Echesa-Imaculate-Zepwa <global-imaculate.zepwa@student.moringaschool.com>
2023-01-11 07:03:30 +0300

    edit searchbar

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,42 @@
+const SearchBar = () => (
+        
+    <div className="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search"/>
+      <button type="submit"><i className="fa fa-search"></i></button>
+    </form>
+  </div>
+
+    
+    
+);
+
+export default SearchBar;
+
+
+
+// const SearchBar = () => (
+    // <form action="/" method="get">
+        {/* <div className="search-bar"> */}
+        {/* <label htmlFor="header-search"> */}
+            {/* <span className="visually-hidden">Search blog posts</span> */}
+{/*  */}
+        {/* </label> */}
+        {/* <input */}
+            // type="text"
+            // id="header-search"
+            // placeholder="search venues"
+            // name="s" 
+        // />
+        {/* <button type="submit">Search</button> */}
+        {/* </div> */}
+    {/* </form> */}
+    // 
+// );
+// 
+// export default SearchBar;
+
+
+
+
+
