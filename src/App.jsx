efef1f9cca25a477b5183fc612c580266a36c1d3
Isaack Mungui <munguiisaack@gmail.com>
2023-01-11 17:55:42 +0300

    Fiexd conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import SearchBar from './components/SearchBar'
-import NavBar from './components/navBar'
 
 function App() {
 
-  return (   
-    <>
- <div classname='App'>
-        <SearchBar/>
-        <NavBar/>
-      </div>
-      <hr></hr>
-      <h2>Our Venues</h2>
-      </>
-
-  )
 }
 
 export default App
