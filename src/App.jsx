e9e24b49f3689e23d13a55a7d1e159c60902ab40
Isaack Mungui <munguiisaack@gmail.com>
2023-01-11 08:35:59 +0300

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
-import NavBar from './components/navbar'
-import Footer from './components/footer'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import LandingPage from './components/LandingPage'
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
+  // const {
+  //   activeMenu,
+  //   themeSettings,
+  //   toggleThemeSettings,
+  //   currentColor,
+  //   currentMode,
+  // } = useStateContext();
   return (
     <div className="App">
       <NavBar/>
+      <Routes>
+        <Route path='/' element={ <LandingPage/> } />
+        <Route path='login' element={ <LogIn /> } />
+        <Route path='/signup' element={ <SignUp /> } />
+        {/* <Route path='/' element={ <Home/> } /> */}
+        <Route path='/contact' element={ <Contact /> } />
+      </Routes>
       <Footer />
     </div>
   )
