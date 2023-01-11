621788ec67a405a2a1f9623c2d9b7a46f9dc022b
Isaack Mungui <munguiisaack@gmail.com>
2023-01-11 18:02:32 +0300

    Revert "Fiexd conflicts"
    This reverts commit efef1f9cca25a477b5183fc612c580266a36c1d3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import SearchBar from './components/SearchBar'
+import NavBar from './components/navBar'
 
 function App() {
 
+  return (   
+    <>
+ <div classname='App'>
+        <SearchBar/>
+        <NavBar/>
+      </div>
+      <hr></hr>
+      <h2>Our Venues</h2>
+      </>
+
+  )
 }
 
 export default App
