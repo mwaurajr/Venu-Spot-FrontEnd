15d616c11d2e41478b6f4752e7197bd5bf3fda80
Isaack Mungui <munguiisaack@gmail.com>
2023-01-16 05:52:34 +0300

    Made changes to landing page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -15,17 +15,16 @@ function LandingPage() {
                     perfect venue to bring life to <br />
                     the events you host.
                 </h4>
-                <img src={Venue} className="w-3/6 h-2/4 absolute mt-80 right-20" alt="" />
+                <img src={Venue} className="w-3/6 h-2/4 absolute mt-80 right-20 sm:w-1/2" alt="" />
             </div>
             <button class="rounded-full float-left absolute left-20 mt-60 bg-green-800"><a href="/signup" className="text-white hover:text-white">Get Started</a></button>
-            <button class="rounded-full float-left absolute left-60 mt-60 bg-gray-200 text-green-800 hover:bg-green-800 hover:text-white"><a href="/signup" className="text-green-800 hover:text-white">Learn More</a></button>
+            <button class="rounded-full float-left absolute left-60 mt-60 bg-gray-200 text-green-800 hover:bg-green-800 text-white"><a href="/signup" className="text-green-800 hover:text-white">Learn More</a></button>
 
-            
-            <h4>Featured Venues</h4>
-            <div className="flex absolute top-80 mt-60 left-40 justify-center items-center">
-                <img src={Weddings} className="w-1/3 h-40 mx-8"/>
-                <img src={Reunion} className="w-1/3 h-40 mx-8"/>
-                <img src={Tropical} className="w-1/3 h-40 mx-8" />
+            <h4 className="absolute flex top-80 left-80 ml-60 mt-60 underline">Featured Venues</h4>
+            <div className="flex absolute top-80 mt-80 left-40 justify-center items-center">            
+                <img src={Weddings} className="cursor-pointer transition-transform duration-200 transform hover:scale-150 w-1/3 h-40 mx-8"/>
+                <img src={Reunion} className="w-1/3 h-40 mx-8 cursor-pointer transition-transform duration-200 transform hover:scale-150"/>
+                <img src={Tropical} className="w-1/3 h-40 mx-8 cursor-pointer transition-transform duration-200 transform hover:scale-150" />
             </div>
         </div>
     )
