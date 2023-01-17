1ab31fb7527c0b7bdfcac3a7950a73b4af57833d
Timothy <timothy.mureithi@student.moringaschool.com>
2023-01-17 19:27:26 +0300

    updated App.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,30 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import Dashboard from './pages0/Dashboard.jsx';
-import MyVenues from './pages0/MyVenues.jsx';
-import Clients from './pages0/Clients';
-import Comment from './pages0/Comment.jsx';
-import EditProfile from './pages0/EditProfile';
-import AddVenue from './pages0/AddVenue.jsx';
-import Cart from './components/Cart';
-import Navigation from './components/Navigation';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WithNavbar from './layouts/WithNavbar';
+import WithOutnavbar from './layouts/WithOutnavbar';
+import Signin from './pages/auth/Signin'
+import Signup from './pages/auth/Signup'
+import Home from './pages/home'
+import About from './pages/about'
+import Users from './pages/users'
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/navigation" element={<Navigation />} />
+    <Router>
+      <Routes>
+        <Route element={<WithOutnavbar/>}>
+          <Route path='/signin' element={<Signin/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+        </Route>
+        <Route element={<WithNavbar/>}>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/users' element={<Users/>}/>
+        </Route>
+      </Routes>
+    </Router>
+  )
+}
 
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/my-venues" element={<MyVenues />} />
-          <Route path="/comment" element={<Comment />} />
-          <Route path="/my-clients" element={<Clients />} />
-          <Route path="/manage-venue" element={<AddVenue />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/payment" element={<Cart />} />
-
-        </Routes>
-      </Sidebar>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+export default App
